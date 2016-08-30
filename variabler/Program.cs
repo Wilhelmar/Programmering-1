84f32d826e404bdc100344665f2fddadb59ae315
wilhelmar <wilhelmar@vgy.se>
2016-08-30 14:12:23 +0200

    variabler commit

--- variabler/Program.cs
+++ variabler/Program.cs
@@ -10,6 +10,48 @@ namespace variabler
     {
         static void Main(string[] args)
         {
+            //Övning 1
+            /*
+            int tal1, tal2;
+            
+            
+
+            Console.Write("mata in tal1:");
+            string input1 = Console.ReadLine();
+            tal1 = int.Parse(input1);
+            Console.Write("mata in tal2:");
+            string input2 = Console.ReadLine();
+            tal2 = int.Parse(input2);
+
+            Console.WriteLine("summa:" + (tal1+tal2));
+            Console.WriteLine("produkten:" + (tal1 * tal2));
+            Console.WriteLine("kvoten:" + (tal1 / tal2));
+
+    
+
+            //Övning 2
+
+            string inputFirstName, inputLastName;
+
+            Console.Write("Vad heter du i förnamn?: ");
+            inputFirstName = Console.ReadLine();
+            Console.Write("Vad heter du i efternamn?: ");
+            inputLastName = Console.ReadLine();
+
+            Console.WriteLine("Hej " + inputFirstName + " " + inputLastName + "!");
+            Console.ReadKey();
+
+   
+
+            //Övning 3
+            int celcius, farenheit;
+
+            Console.WriteLine("Mata in grader i celcius: ");
+            string inputCelcius = Console.ReadLine();
+            celcius = int.Parse(inputCelcius);
+            Console.WriteLine("Det är " + (celcius * 1.8 + 32) + " " + "grader farenheit");
+
+     */
         }
     }
 }
